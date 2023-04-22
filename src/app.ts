9639e52486ad234351ae6ecda9dd99ae6c1784f1
yeha98555 <yeha98555@gmail.com>
2023-04-23 00:23:48 +0800

    build: Install typescript

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import createError from "http-errors";
+import express, { Request, Response, NextFunction } from "express";
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import indexRouter from "./routes/index";
+
+const app: express.Application = express();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send("error");
+});
+
+module.exports = app;
