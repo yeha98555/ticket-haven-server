51699f46ded2e033d8cdba73dffee4b0febefba5
Yury <yuury.chang@gmail.com>
2023-04-29 12:31:40 +0800

    style: Format all file

--- src/exceptions/HttpException.ts
+++ src/exceptions/HttpException.ts
@@ -3,7 +3,7 @@ export class HttpException extends Error {
 
   public message: string;
 
-  constructor (status: number, message: string) {
+  constructor(status: number, message: string) {
     super(message);
     this.status = status;
     this.message = message;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,11 +5,12 @@ import { swaggerSpec } from './swagger/swagger';
 const router = Router();
 
 const headers = {
-  'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+  'Access-Control-Allow-Headers':
+    'Content-Type, Authorization, Content-Length, X-Requested-With',
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'PATCH, POST, GET, OPTIONS, DELETE',
-  'Content-Type': 'application/json'
-}
+  'Content-Type': 'application/json',
+};
 
 /* GET home page. */
 /**
@@ -40,10 +41,12 @@ const headers = {
  */
 router.get('/', (req: Request, res: Response) => {
   res.writeHead(200, headers);
-  res.write(JSON.stringify({
-    "status": "success",
-    "message": "Welcome to the API"
-  }));
+  res.write(
+    JSON.stringify({
+      status: 'success',
+      message: 'Welcome to the API',
+    }),
+  );
   res.end();
   // res.send('Welcome to the API');
 });
