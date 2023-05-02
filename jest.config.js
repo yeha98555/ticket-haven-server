438de3d2e0af283f43e09dd88b3d038b2178f374
Yury <yuury.chang@gmail.com>
2023-05-02 12:23:14 +0800

    chore: Fix paths mapping

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
 module.exports = {
   coverageDirectory: 'coverage',
   preset: 'ts-jest',
   testEnvironment: 'node',
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 };
