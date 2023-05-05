a0e6e9c5ad5da86895fafe4d761261f6fd925d48
Yury <yuury.chang@gmail.com>
2023-05-05 23:08:39 +0800

    feat: Update User model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,19 +1,40 @@
+import { Gender } from '@/enums/gender';
 import { Schema, model } from 'mongoose';
+import z from 'zod';
 
-const userSchema = new Schema(
+export interface IUser {
+  uid?: string;
+  username?: string;
+  email?: string;
+  phone?: string;
+  gender?: Gender;
+  email_verify?: boolean;
+  phone_verify?: boolean;
+  password?: string;
+  bank_code?: string;
+  bank_account?: string;
+  avatar_url?: string;
+  activity_region?: number;
+  delete_at?: Date;
+}
+
+const userSchema = new Schema<IUser>(
   {
     uid: String,
     username: String,
     email: String,
     phone: String,
-    gender: Number,
+    gender: {
+      type: Number,
+      validate: z.nativeEnum(Gender).optional().parse,
+    },
     email_verify: Boolean,
     phone_verify: Boolean,
     password: String,
     bank_code: String,
     bank_account: String,
     avatar_url: String,
-    activity_area: Number,
+    activity_region: Number,
     delete_at: Date,
   },
   {
@@ -24,6 +45,6 @@ const userSchema = new Schema(
   },
 );
 
-const UserModel = model('user', userSchema);
+const UserModel = model<IUser>('user', userSchema);
 
 export default UserModel;
