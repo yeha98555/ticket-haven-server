960628264407330dc7330d522ce44136b04e700a
PageShih <a0981863141@gmail.com>
2023-05-08 19:54:17 +0800

    refactor: conding style

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -4,9 +4,7 @@ import userService from '@/services/user'
 const userController = {
   signup: async (req: Request, res: Response) => {
     try {
-      const data = await userService.signup(req.body);
-      res.status(200);
-      res.send(data);
+      await userService.signup(req.body);
     } catch (error) {
       res.status(500);
       res.send({ message: '好像哪裡出錯了!' });
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,12 +1,19 @@
+import User from '@/models/user';
 
 const userService = {
   signin: async ({email, password}: {email: string, password: string}) => {
     console.log(email, password);
     return {message: '登入成功！'}
   },
-  signup: async ({userName, email, password}: {userName: string, email: string, password: string}) => {
-    console.log(userName, email, password);
-    return {message: '註冊成功，請重新登入'}
+  signup: async (req: {userName: string, email: string, password: string}) => {
+    const newUser = new User(req);
+    const message = await newUser.save().then(() => {
+      return Promise.resolve({status: 200, message: '註冊成功，請重新登入'});
+    })
+    .catch((error: Error)=>{
+      return Promise.reject({status: 500, message: error.message});
+    });
+    return message
   }
 }
 
