d93df2183d1a942e38e355b96d74e54defd1409a
Yury <yuury.chang@gmail.com>
2023-05-13 03:14:29 +0800

    feat: Don't create log file if DIR is not set

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -2,20 +2,6 @@ import path from 'path';
 import winston, { format } from 'winston';
 import DailyRotateFile from 'winston-daily-rotate-file';
 
-const fileTransport: DailyRotateFile = new DailyRotateFile({
-  filename: path.join(process.env.LOG_FILE_DIR, '%DATE%.log'),
-  datePattern: 'YYYY-MM-DD',
-  format: format.combine(
-    format.uncolorize(),
-    format.timestamp(),
-    format.printf((info) => {
-      return `${info.timestamp} ${info.level}: ${info.message}`;
-    }),
-  ),
-  maxSize: '20m',
-  maxFiles: '14d',
-});
-
 const ConsoleTransport = new winston.transports.Console({
   format: format.combine(
     format.colorize(),
@@ -28,10 +14,26 @@ const ConsoleTransport = new winston.transports.Console({
 const logger = winston.createLogger({
   level: 'http',
   transports: [
-    process.env.NODE_ENV === 'production' && fileTransport,
     process.env.NODE_ENV === 'development' && ConsoleTransport,
   ].filter(Boolean) as winston.transport[],
 });
+
+if (process.env.LOG_FILE_DIR) {
+  const fileTransport: DailyRotateFile = new DailyRotateFile({
+    filename: path.join(process.env.LOG_FILE_DIR, '%DATE%.log'),
+    datePattern: 'YYYY-MM-DD',
+    format: format.combine(
+      format.uncolorize(),
+      format.timestamp(),
+      format.printf((info) => {
+        return `${info.timestamp} ${info.level}: ${info.message}`;
+      }),
+    ),
+    maxSize: '20m',
+    maxFiles: '14d',
+  });
+  logger.add(fileTransport);
+}
 
 export const logError = (err: Error, level = 'error') => {
   logger.log({
