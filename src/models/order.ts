4367099376918b68ed9a71266524f60f0abbce5b
Peter <yuury.chang@gmail.com>
2023-05-20 17:42:43 +0800

    feat: Add ticket & order model

--- src/models/order.ts
+++ src/models/order.ts
@@ -6,6 +6,14 @@ const orderSchema = new Schema(
     activity_id: { type: Types.ObjectId, required: true },
     event_id: { type: Types.ObjectId, required: true },
     seat_reservation_id: { type: Types.ObjectId, required: true },
+    order_no: { type: String, required: true, unique: true },
+    transfer_from_order: Types.ObjectId,
+    original_ticket_ids: [
+      {
+        type: Types.ObjectId,
+        validate: (v: unknown) => Array.isArray(v) && v.length > 0,
+      },
+    ],
   },
   {
     timestamps: {
