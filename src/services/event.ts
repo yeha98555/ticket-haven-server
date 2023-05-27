5967a52dc0778911b32c2e59f57a75542abd199f
yeha98555 <yeha98555@gmail.com>
2023-05-27 12:06:09 +0800

    feat: Add filtering purchased seats

--- src/services/event.ts
+++ src/services/event.ts
@@ -1,5 +1,6 @@
 import { NotFoundException } from "@/exceptions/NotFoundException";
 import ActivityModel from "@/models/activity";
+import TicketModel from "@/models/ticket";
 import { Types } from "mongoose";
 
 const eventService = {
@@ -13,20 +14,46 @@ const eventService = {
 
     if (!activity) throw new NotFoundException();
 
-    // TODO: filter purchased seats
+    // Fetch purchased seats from the database
+    const purchasedSeats = await TicketModel.aggregate([
+      {
+        $match: {
+          subarea_id: { $in: activity.areas.flatMap(area => area.subareas.map(subarea => subarea._id)) },
+        }
+      },
+      {
+        $group: {
+          _id: '$subarea_id',
+          purchasedSeats: { $sum: 1 }
+        }
+      }
+    ]);
 
-    return {
-      seat_small_img_url: activity.seat_small_img_url,
-      seats: activity.areas.map(area => ({
-        id: area._id,
-        name: area.name,
-        price: area.price,
-        subAreas: area.subareas.map(subarea => ({
+    // Convert purchasedSeats to a map for easier lookup
+    const purchasedSeatsMap = purchasedSeats.reduce((acc, { _id, purchasedSeats }) => {
+      acc[_id.toString()] = purchasedSeats;
+      return acc;
+    }, {});
+
+    // Generate seats structure and calculate availableSeats
+    const seats = activity.areas.map(area => ({
+      id: area._id,
+      name: area.name,
+      price: area.price,
+      subAreas: area.subareas.map(subarea => {
+        const totalSeats = subarea.rows.reduce((acc, cur) => acc + cur, 0);
+        const purchased = subarea._id ? (purchasedSeatsMap[subarea._id.toString()] || 0) : 0;
+        return {
           id: subarea._id,
           name: subarea.name,
-          remainingSeats: subarea.rows.reduce((acc, cur) => acc + cur, 0),
-        })),
-      })),
+          remainingSeats: totalSeats - purchased,
+        };
+      }),
+    }));
+
+    return {
+      seatImgUrl: activity.seat_small_img_url,
+      seats: seats,
     };
   },
 };
