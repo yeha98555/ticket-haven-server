55da3c77923d5baccd4bbb9d69df678206136527
yeha98555 <yeha98555@gmail.com>
2023-05-28 21:46:50 +0800

    feat: Add event sold out status

--- src/services/activity/index.ts
+++ src/services/activity/index.ts
@@ -1,6 +1,7 @@
 import ActivityModel from '@/models/activity';
 import searchActivities from './searchActivities';
 import { NotFoundException } from '@/exceptions/NotFoundException';
+import TicketModel from '@/models/ticket';
 
 const activityService = {
   searchActivities,
@@ -9,13 +10,39 @@ const activityService = {
     const activity = await ActivityModel.findById(id);
     if (!activity) throw new NotFoundException();
 
+    // Pre-convert event IDs to strings for subsequent queries and comparisons
+    const eventIds = activity.events.map(event => event._id!.toString());
+
+    // Fetch purchased seats from the database
+    const purchasedSeats = await TicketModel.aggregate([
+      {
+        $match: {
+          event_id: { $in: eventIds },
+        }
+      },
+      {
+        $group: {
+          _id: '$event_id',
+          purchasedSeats: { $sum: 1 }
+        }
+      }
+    ]);
+
+    // Convert purchasedSeats to a map for easier lookup
+    const purchasedSeatsMap = purchasedSeats.reduce((acc, { _id, purchasedSeats }) => ({
+      ...acc,
+      [_id.toString()]: purchasedSeats,
+    }), {});
+
     const returnEvents = activity.events.map((e) => {
+      const eventId = e._id!.toString();
       return {
-        id: e._id,
+        id: eventId,
         startTime: e.start_at,
         endTime: e.end_at,
         sellStartTime: e.sell_at,
         sellEndTime: e.sellend_at,
+        soldOut: purchasedSeatsMap[eventId] === activity.seat_total,
       };
     });
 
