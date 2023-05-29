0a41013927211d8104c8ebce6f855ac9754b0165
yeha98555 <yeha98555@gmail.com>
2023-05-29 16:58:24 +0800

    Revert "fix: Update error message"
    This reverts commit 89757e5ef51f916e70fa2e4feba4cadebd2f9e77.

--- src/services/activity/index.ts
+++ src/services/activity/index.ts
@@ -2,8 +2,6 @@ import ActivityModel from '@/models/activity';
 import searchActivities from './searchActivities';
 import { NotFoundException } from '@/exceptions/NotFoundException';
 import TicketModel from '@/models/ticket';
-import { appError } from '../appError';
-import { StatusCode } from '@/enums/statusCode';
 
 const activityService = {
   searchActivities,
@@ -11,7 +9,7 @@ const activityService = {
   getActivityInfo: async (id: string) => {
     const activity = await ActivityModel.findById(id).select('-__v -create_at -update_at -deleted_at -seat_small_img_url -region -areas -events.qrcode_verify_link -events.create_at -events.update_at');
     if (!activity) throw new NotFoundException();
-    if (!activity.is_published) throw appError(404, StatusCode.NOT_FOUND, 'Activity not published');
+    if (!activity.is_published) throw new NotFoundException();
 
     // Pre-convert event IDs to strings for subsequent queries and comparisons
     const eventIds = activity.events.map(event => event._id!.toString());
