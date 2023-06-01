f29cd074772759dca4d1f84577261c6385a4d229
Yury <yuury.chang@gmail.com>
2023-06-01 23:51:26 +0800

    fix: TicketCount calculate

--- src/services/order/payment.ts
+++ src/services/order/payment.ts
@@ -1,14 +1,21 @@
 import crypto from 'crypto';
-import { NotFoundException } from "@/exceptions/NotFoundException";
-import { Activity } from "@/models/activity";
-import OrderModel from "@/models/order";
-import { User } from "@/models/user";
+import { NotFoundException } from '@/exceptions/NotFoundException';
+import { Activity } from '@/models/activity';
+import OrderModel from '@/models/order';
+import { User } from '@/models/user';
 import { appError } from '../appError';
 import { StatusCode } from '@/enums/statusCode';
-import TicketModel from '@/models/ticket';
+import SeatReservationModel from '@/models/seatReservation';
 
 const RespondType = 'JSON';
-const { NEWEBPAY_VERSION, NEWEBPAY_MERCHANT_ID, NEWEBPAY_HASH_KEY, NEWEBPAY_HASH_IV, NEWEBPAY_RETURN_URL, NEWEBPAY_NOTIFY_URL } = process.env;
+const {
+  NEWEBPAY_VERSION,
+  NEWEBPAY_MERCHANT_ID,
+  NEWEBPAY_HASH_KEY,
+  NEWEBPAY_HASH_IV,
+  NEWEBPAY_RETURN_URL,
+  NEWEBPAY_NOTIFY_URL,
+} = process.env;
 
 interface NewebPayPaymentRequest {
   MerchantOrderNo: string;
@@ -26,26 +33,48 @@ const formatDesc = (name: string, quantity: number, start_at: Date) => {
 };
 
 const genDataChain = (paymentData: NewebPayPaymentRequest) => {
-  return `MerchantID=${NEWEBPAY_MERCHANT_ID}&RespondType=${RespondType}&TimeStamp=${paymentData.TimeStamp}&Version=${NEWEBPAY_VERSION}&MerchantOrderNo=${paymentData.MerchantOrderNo}&Amt=${paymentData.Amt}&ItemDesc=${encodeURIComponent(paymentData.ItemDesc).replace(/%20/g, '+')}&Email=${encodeURIComponent(paymentData.Email)}&ReturnURL=${NEWEBPAY_RETURN_URL}&NotifyURL=${NEWEBPAY_NOTIFY_URL}`;
-}
+  return `MerchantID=${NEWEBPAY_MERCHANT_ID}&RespondType=${RespondType}&TimeStamp=${
+    paymentData.TimeStamp
+  }&Version=${NEWEBPAY_VERSION}&MerchantOrderNo=${
+    paymentData.MerchantOrderNo
+  }&Amt=${paymentData.Amt}&ItemDesc=${encodeURIComponent(
+    paymentData.ItemDesc,
+  ).replace(/%20/g, '+')}&Email=${encodeURIComponent(
+    paymentData.Email,
+  )}&ReturnURL=${NEWEBPAY_RETURN_URL}&NotifyURL=${NEWEBPAY_NOTIFY_URL}`;
+};
 
 const createAesEncrypt = (tradeInfo: NewebPayPaymentRequest) => {
   if (!NEWEBPAY_HASH_KEY || !NEWEBPAY_HASH_IV) {
-    throw appError(500, StatusCode.SERVER_ERROR, 'NEWEBPAY_HASH_KEY or NEWEBPAY_HASH_IV not found')
+    throw appError(
+      500,
+      StatusCode.SERVER_ERROR,
+      'NEWEBPAY_HASH_KEY or NEWEBPAY_HASH_IV not found',
+    );
   }
-  const encrypt = crypto.createCipheriv('aes-256-cbc', NEWEBPAY_HASH_KEY, NEWEBPAY_HASH_IV);
-  const encrypted = encrypt.update(genDataChain(tradeInfo), 'utf8', 'hex') + encrypt.final('hex');
+  const encrypt = crypto.createCipheriv(
+    'aes-256-cbc',
+    NEWEBPAY_HASH_KEY,
+    NEWEBPAY_HASH_IV,
+  );
+  const encrypted =
+    encrypt.update(genDataChain(tradeInfo), 'utf8', 'hex') +
+    encrypt.final('hex');
   return encrypted;
-}
+};
 
 const createShaEncrypt = (aesEncrypt: string) => {
   if (!NEWEBPAY_HASH_KEY || !NEWEBPAY_HASH_IV) {
-    throw appError(500, StatusCode.SERVER_ERROR, 'NEWEBPAY_HASH_KEY or NEWEBPAY_HASH_IV not found')
+    throw appError(
+      500,
+      StatusCode.SERVER_ERROR,
+      'NEWEBPAY_HASH_KEY or NEWEBPAY_HASH_IV not found',
+    );
   }
   const sha = crypto.createHash('sha256');
   const plainText = `HashKey=${NEWEBPAY_HASH_KEY}&${aesEncrypt}&HashIV=${NEWEBPAY_HASH_IV}`;
   return sha.update(plainText).digest('hex').toUpperCase();
-}
+};
 
 const payment = async (userId: string, orderNo: string) => {
   const order = await OrderModel.findOne({
@@ -59,17 +88,22 @@ const payment = async (userId: string, orderNo: string) => {
   if (!order) throw new NotFoundException();
 
   // Calculate the number of tickets
-  const ticketCount = await TicketModel.countDocuments({
-    order_id: order._id,
-  });
+  const seatReservation = await SeatReservationModel.findById(
+    order.seat_reservation_id,
+  );
+  const ticketCount = seatReservation!.seats.length;
 
   const paymentData = {
-    'MerchantOrderNo': order.order_no,
-    'RespondType': RespondType,
-    'TimeStamp': Math.floor(Date.now() / 1000),
-    'Email': order.user_id.email,
-    'Amt': order.price,
-    'ItemDesc': formatDesc(order.activity_id.name, ticketCount, order.activity_id.start_at),
+    MerchantOrderNo: order.order_no,
+    RespondType: RespondType,
+    TimeStamp: Math.floor(Date.now() / 1000),
+    Email: order.user_id.email,
+    Amt: order.price,
+    ItemDesc: formatDesc(
+      order.activity_id.name,
+      ticketCount,
+      order.activity_id.start_at,
+    ),
   };
 
   const aesEncrypt = createAesEncrypt(paymentData);
